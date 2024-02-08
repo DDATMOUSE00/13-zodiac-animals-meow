a14e9129f5dfefdc0253a3e60b93c4d181c024d1
suhyunChun <suhyun.chun@stonybrook.edu>
2024-02-08 15:09:31 +0900

    [Add] add featrue showing all quests / processing quests

--- Assets/02.Scripts/09.GameManager/QuestManager.cs
+++ Assets/02.Scripts/09.GameManager/QuestManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class QuestManager : MonoBehaviour
@@ -9,13 +10,40 @@ public class QuestManager : MonoBehaviour
     public static QuestManager I; 
     private Dictionary<int, List<Quest>> questList;
     public Dictionary<string, Quest> allQuests;
+    public Transform QuestContainerUI;
+    public GameObject questUI;
+    public GameObject backBtn;
+
+    private int PlayerLevel = 1; // 나중에 바깥에서 가져올 정보.
  
     private void Awake()
     {
         I = this;
-        questList = CreateQuestLIst();
+    //    questList = CreateQuestLIst();
+        allQuests = getAllQuests();
     }
 
+    private Dictionary<string, Quest> getAllQuests()
+    {
+        QuestInfo[] allQuests = Resources.LoadAll<QuestInfo>("Quests");
+        Dictionary<string, Quest> AllQuests= new Dictionary<string, Quest>();
+
+        foreach (QuestInfo q in allQuests)
+        {
+            if (AllQuests.ContainsKey(q.QuestId))
+            {
+                Debug.Log("key is duplicated");
+            }
+            else
+            {
+                Quest newQ = new Quest(q);
+                if (PlayerLevel >= newQ.q.levelRequirement)
+                    newQ.state = QuestState.CAN_START;  
+                AllQuests.Add(newQ.q.QuestId, newQ);
+            }
+        }
+        return AllQuests;
+    }
     private Dictionary<int, List<Quest>> CreateQuestLIst()
     {
         QuestInfo[] allQuests = Resources.LoadAll<QuestInfo>("Quests");
@@ -46,6 +74,48 @@ public class QuestManager : MonoBehaviour
             }
         }
         return null;
+    }
+
+    private void ClearAllChildUnderContent()
+    {
+        foreach(Transform t in QuestContainerUI)
+        {
+            Destroy(t.gameObject);
+        }
+    }
+    public void RefreshAllQuest()
+    {
+        if (QuestContainerUI.childCount != 0)
+            ClearAllChildUnderContent();
+        questUI.SetActive(true);
+        backBtn.SetActive(false);
+        List<Quest> tmp = allQuests.Values.ToList();
+        foreach (var q in tmp)
+        {
+            if(q.state == QuestState.CAN_START)
+            {
+                GameObject questPrefab = Resources.Load("QuestSlot") as GameObject;
+                GameObject newQuest = Instantiate(questPrefab, QuestContainerUI);
+                QuestSlot qslot = newQuest.GetComponent<QuestSlot>();
+                qslot.quest = q;
+                qslot.Setting();
+            }
+        }
+    }
+
+    public void RefreshProgressingQuest()
+    {
+        if(QuestContainerUI.childCount != 0)
+            ClearAllChildUnderContent();
+        backBtn.SetActive(true);
+        List<Quest> tmp = allQuests.Values.ToList();
+        foreach (var q in tmp)
+        {
+            if (q.state == QuestState.IN_PROGRESS)
+            {
+
+            }
+        }
     }
 }
 
