8257bf902e6ac6aab8ae34a2683de36afd8fbb5f
DDATMOUSE00 <ddatmouse00@gmail.com>
2024-02-24 04:20:39 +0900

    []
    []

--- Assets/04.Extends/2D Cute Domestic Animal Pack V.2/Scripts/Acting.cs
+++ Assets/04.Extends/2D Cute Domestic Animal Pack V.2/Scripts/Acting.cs
@@ -3,11 +3,8 @@
     Eat,
     Eat2,
     Idle,
-    Idle2,
     Jump,
-    Jump2,
     Sleep,
-    Sleep2,
     Walk,
-    Walk2,
+    Idle2,
 }
--- Assets/04.Extends/2D Cute Domestic Animal Pack V.2/Scripts/CharacterAnimation.cs
+++ Assets/04.Extends/2D Cute Domestic Animal Pack V.2/Scripts/CharacterAnimation.cs
@@ -17,13 +17,10 @@ public abstract class CharacterAnimation : MonoBehaviour
     public void Eat() => SetAnimation(Acting.Eat);
     public void Eat2() => SetAnimation(Acting.Eat2);
     public void Idle() => SetAnimation(Acting.Idle);
-    public void Idle2() => SetAnimation(Acting.Idle2);
     public void Jump() => SetAnimation(Acting.Jump);
-    public void Jump2() => SetAnimation(Acting.Jump2);
     public void Sleep() => SetAnimation(Acting.Sleep);
-    public void Sleep2() => SetAnimation(Acting.Sleep2);
     public void Walk() => SetAnimation(Acting.Walk);
-    public void Walk2() => SetAnimation(Acting.Walk2);
+    public void Idle2() => SetAnimation(Acting.Idle2);
 
 
     public void PlayAnimationComplete()
