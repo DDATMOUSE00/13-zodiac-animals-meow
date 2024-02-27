e36ad4b826000a5625859643d1deeb5c6f329779
DDATMOUSE00 <ddatmouse00@gmail.com>
2024-02-27 21:53:54 +0900

    [Add] 튜토리얼 추가
    튜토리얼 추가

--- Assets/02.Scripts/06.UI/TalkUI.cs
+++ Assets/02.Scripts/06.UI/TalkUI.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 [System.Serializable]
@@ -13,16 +15,17 @@ public class Talk
 
 public class TalkUI : MonoBehaviour
 {
-    [SerializeField] Text txt_Talk;
+    [SerializeField] TMP_Text txt_Talk;
     [SerializeField] SpriteRenderer Sprite_TalkBox;
+    //[SerializeField] private TutorialManager _TutorialManager;
 
     private bool IsTalk = false;
-
+    public int ShowTextCount = 1;
     private int count = 0;
 
     [SerializeField] private Talk[] talk;
 
-    public void ShowTalk()
+    public void ShowTalk1()
     {
         OnOff(true);
 
@@ -30,6 +33,13 @@ public class TalkUI : MonoBehaviour
         NextTalk();
     }
 
+    public void ShowTalk2()
+    {
+        OnOff(true);
+
+        count = 0;
+        NextTalk();
+    }
     private void OnOff(bool TF)
     {
         Sprite_TalkBox.gameObject.SetActive(TF);
@@ -55,7 +65,21 @@ public class TalkUI : MonoBehaviour
                 }
                 else
                 {
+                    ShowTextCount++;
                     OnOff(false);
+                    if (ShowTextCount == 2)
+                    {
+                        SceneManager.LoadScene("TutorialScene2");
+                    }
+                    if (ShowTextCount == 3)
+                    {
+                        SceneManager.LoadScene("TutorialScene3");
+                    }
+                    if (ShowTextCount == 4)
+                    {
+                        SceneManager.LoadScene("Village_FINAL");
+                        GameManager.Instance.player.transform.position = new Vector3(0, 0, -20);
+                    }
                 }
             }
         }
